e72b3425b4c46a152af73311e943cabcfbb73b69
Helge Holgersson <sebastian.holgersson@gmail.com>
2013-05-30 17:04:53 +0200

    Kommentering finns nu i rmi-paketet, tog även bort data.java ur både servern och klienten eftersom den inte ska användas

--- Client/src/rmi/Api.java
+++ Client/src/rmi/Api.java
@@ -1,16 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rmi;
 
 import java.rmi.*;
+
 /**
- *
- * @author jasv0004
+ * 
+ * @author Helge Holgersson, Jacob Svedman, Andreas Forsbom
+ * Detta är det gemensamma API som delas med Serverapplikationen och klientapplikationen
+ * Detta interface beskriver vilka meotder som klienten kan använda på Serverapplikationen.
+ * Det är viktigt att denna klass finns i samma version i bägge program.
+ * 
+ * Ett objekt av denna klass kommer skapas av Servern och en referens till det kommer att läggas till i RMI-registret.
  */
-public interface Api extends Remote {
-    public Data incrementCounter(Data value) throws RemoteException;
-    public Data testapi(Data value) throws RemoteException;
-    public sortData quickSort(sortData value)throws RemoteException;
+public interface Api extends Remote
+{
+    //public Data incrementCounter(Data value) throws RemoteException;
+    public sortData quickSort(sortData value) throws RemoteException;
 }
+
--- Server/src/rmi/Api.java
+++ Server/src/rmi/Api.java
@@ -2,6 +2,15 @@ package rmi;
 
 import java.rmi.*;
 
+/**
+ * 
+ * @author Helge Holgersson, Jacob Svedman, Andreas Forsbom
+ * Detta är det gemensamma API som delas med Serverapplikationen och klientapplikationen
+ * Detta interface beskriver vilka meotder som klienten kan använda på Serverapplikationen.
+ * Det är viktigt att denna klass finns i samma version i bägge program.
+ * 
+ * Ett objekt av denna klass kommer skapas av Servern och en referens till det kommer att läggas till i RMI-registret.
+ */
 public interface Api extends Remote
 {
     //public Data incrementCounter(Data value) throws RemoteException;
